32082b3738d670a7ceb1ce481f70c7265266eb77
cicwak <vapvapapvpva@yandex.ru>
2023-07-24 15:55:46 +0300

    feat: validator redis host and port

--- core/config.py
+++ core/config.py
@@ -1,10 +1,15 @@
+import typing
+from urllib.parse import urlparse
+
 from pydantic import BaseSettings, Field, validator
 
 
 class Settings(BaseSettings):
     DATABASE_URL: str = Field(default=None, env='DATABASE_URL')
 
-    REDIS_URL: str = Field(default=None, env="REDIS_URL")
+    REDIS_URL: typing.Optional[str] = Field(default=None, env="REDIS_URL")
+    REDIS_HOST: typing.Optional[str] = Field(default=None, env="REDIS_URL")
+    REDIS_PORT: typing.Optional[str] = Field(default=None, env="REDIS_URL")
 
     TG_BOT_TOKEN: str = Field(default="", env='TG_BOT_TOKEN')
 
@@ -16,6 +21,14 @@ class Settings(BaseSettings):
         @validator("DEBUG")
         def validator_database_url(cls, v) -> bool:
             return v == "True"
+
+        @validator("REDIS_HOST")
+        def validator_redis_host(cls, v):
+            return urlparse(v).hostname
+
+        @validator("REDIS_PORT")
+        def validator_redis_port(cls, v):
+            return urlparse(v).port
 
 
 settings = Settings()
